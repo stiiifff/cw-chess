55e6952e8facd4c3b443691f069b362f4f8234ba
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-03-16 16:50:56 +0100

    Implement cw2; save admin in storage (#2)

--- src/contract.rs
+++ src/contract.rs
@@ -1,25 +1,30 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::ADMIN;
 
-/*
-// version info for migration info
+// Version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw-chess";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
+    info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    ADMIN.save(deps.storage, &info.sender)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -38,4 +43,19 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+
+    #[test]
+    fn proper_initialization() {
+        let mut deps = mock_dependencies();
+
+        let msg = InstantiateMsg {};
+        let info = mock_info("admin", &[]);
+
+        // we can just call .unwrap() to assert this was a success
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(0, res.messages.len());
+    }
+}
---
+++ src/integration_tests.rs
@@ -0,0 +1,67 @@
+#[cfg(test)]
+mod tests {
+    use crate::helpers::CwChessContract;
+    use crate::msg::InstantiateMsg;
+    use cosmwasm_std::{Addr, Empty};
+    // use cosmwasm_std::{Addr, Coin, Empty, Uint128};
+    use cw_multi_test::{App, AppBuilder, Contract, ContractWrapper, Executor};
+
+    pub fn chess_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(
+            crate::contract::execute,
+            crate::contract::instantiate,
+            crate::contract::query,
+        );
+        Box::new(contract)
+    }
+
+    // const USER: &str = "USER";
+    const ADMIN: &str = "ADMIN";
+    // const NATIVE_DENOM: &str = "denom";
+
+    fn mock_app() -> App {
+        AppBuilder::new().build(|_router, _, _storage| {
+            // router
+            //     .bank
+            //     .init_balance(
+            //         storage,
+            //         &Addr::unchecked(USER),
+            //         vec![Coin {
+            //             denom: NATIVE_DENOM.to_string(),
+            //             amount: Uint128::new(1),
+            //         }],
+            //     )
+            //     .unwrap();
+        })
+    }
+
+    fn proper_instantiate() -> (App, CwChessContract) {
+        let mut app = mock_app();
+        let cw_chess_id = app.store_code(chess_contract());
+
+        let msg = InstantiateMsg {};
+        let cw_chess_contract_addr = app
+            .instantiate_contract(
+                cw_chess_id,
+                Addr::unchecked(ADMIN),
+                &msg,
+                &[],
+                "cw-chess",
+                Some(Addr::unchecked(ADMIN).into_string()),
+            )
+            .unwrap();
+
+        let cw_template_contract = CwChessContract(cw_chess_contract_addr);
+
+        (app, cw_template_contract)
+    }
+
+    mod init {
+        use super::*;
+
+        #[test]
+        fn instantiate() {
+            let (_app, _contract) = proper_instantiate();
+        }
+    }
+}
