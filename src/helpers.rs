926d3d5d85dd30af2e8f506c302f57c45cf59a3e
Steve Degosserie <723552+stiiifff@users.noreply.github.com>
2024-03-16 16:48:05 +0100

    Implement cw2; save admin in storage

--- src/helpers.rs
+++ src/helpers.rs
@@ -5,12 +5,12 @@ use cosmwasm_std::Addr;
 // use cosmwasm_std::{to_json_binary, Addr, CosmosMsg, StdResult, WasmMsg};
 // use crate::msg::ExecuteMsg;
 
-/// CwTemplateContract is a wrapper around Addr that provides a lot of helpers
+/// CwChessContract is a wrapper around Addr that provides a lot of helpers
 /// for working with this.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct CwTemplateContract(pub Addr);
+pub struct CwChessContract(pub Addr);
 
-impl CwTemplateContract {
+impl CwChessContract {
     pub fn addr(&self) -> Addr {
         self.0.clone()
     }
--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,5 @@
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
 
+// Contract admin address
+pub const ADMIN: Item<Addr> = Item::new("contract_admin");
